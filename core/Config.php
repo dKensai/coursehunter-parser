ca92f88ea147d61fb3050a5d5925ce767d054164
Just Me <some@mail.com>
2019-06-11 17:56:41 +0300

    Parser

---
+++ core/Config.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace Core;
+
+/**
+ * CONFIGURATIONS
+ *
+ * PHP version 7.0
+ */
+class Config
+{
+
+    /**
+     * Hide-show errors on the screen ( browser console )
+     * false by default
+     * @var boolean
+     */
+    const SHOW_ERRORS = false;
+
+
+    /**
+     * Main folder to save
+     * @var string
+     */
+    const BASE_DIR = '../DUMP/';
+
+
+    /**
+     * Language of messages - ru, en
+     * @var string
+     */
+    const LANG = 'en';
+
+
+    /**
+     * Limit of proxy ip to use
+     * when limit is end, script will stop 
+     * 20 by default
+     * @var integer
+     */
+    const IP_LIMIT = 20;
+}
---
+++ public/js/app.js
@@ -0,0 +1,88 @@
+// localization: ru, en
+var LANG = 'en';
+
+
+$.getJSON("../data/local-front.json", function(data) {
+    for (var i in data[LANG]) {
+        window[i] = data[LANG][i];
+    }
+    $('form').find('.download_text').text(paste_link).end()
+        .find('input[name=url]').attr('placeholder', link_of_course).end()
+        .find('button').text(download_btn_text);
+    $('.jumbotron').find('h3').text(has_loaded);
+    parserForm();
+})
+
+
+function parserForm() {
+
+    $('form').on('submit', function(e) {
+
+        e.preventDefault();
+
+        var $dt = $('form').find('.download_text');
+        var $di = $('form').find('.download_img').children('img');
+
+        $dt.fadeOut(function() {
+
+            $dt.html(is_loading).end().fadeIn()
+
+        });
+
+        $di.fadeIn();
+
+        $('.jumbotron').fadeOut();
+
+
+        $.ajax({
+            type: "POST",
+            url: "index.php",
+            data: $('form').serialize(),
+            success: function(data) {
+                console.log('data: ', data);
+
+                if (data[0] === true) {
+
+                    $dt.fadeOut(function() {
+
+                        $dt.html(paste_link).end().fadeIn()
+
+                    });
+
+                    $di.fadeOut();
+
+                    $('.jumbotron').find('p:eq(0)').html(data[1]['title']).end()
+                        .find('p:eq(1)').html(data[1]['info']).end()
+                        .find('p:eq(2)').html(data[1]['samples']);
+
+                    var li = '';
+
+                    var videos = data[1]['videos'];
+
+                    for (var i in videos) {
+
+                        li += '<li>' + videos[i] + '</li>'
+
+                    };
+
+                    $('.jumbotron').find('ul').html(li).end().fadeIn();
+
+                    $('input[name=url]').val('');
+
+                } else {
+
+                    alert(server_error);
+
+                }
+
+            },
+            error: function(error) {
+                console.log(error);
+                console.log(error.responseText);
+            },
+        });
+
+    })
+
+
+}
